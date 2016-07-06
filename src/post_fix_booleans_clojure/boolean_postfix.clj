9cf4de1cd933420e6c5cfea5fe1ffdaaf9f7da66
tprior <tprior@newsweaver.com>
2016-07-06 17:28:47 +0100

    extra tests and refactor to use destructuring

--- src/post_fix_booleans_clojure/boolean_postfix.clj
+++ src/post_fix_booleans_clojure/boolean_postfix.clj
@@ -1,23 +1,16 @@
 (ns post-fix-booleans-clojure.boolean-postfix)
 
-(defn push-true-val-to-stack [stack] (conj stack true))
+(defn ^:private push-true-val-to-stack [stack] (conj stack true))
 
-(defn push-false-val-to-stack [stack] (conj stack false))
+(defn ^:private push-false-val-to-stack [stack] (conj stack false))
 
-(defn postfix-or [stack]
-  (conj (rest (rest stack)) (or (first stack) (first (rest stack)))))
+(defn ^:private postfix-or [[b1 b2 & t]] (conj t (or b1 b2)))
 
-(defn postfix-and [stack]
-  (conj (rest (rest stack)) (and (first stack) (first (rest stack)))))
+(defn ^:private postfix-and [[b1 b2 & t]] (conj t (and b1 b2)))
 
-(defn postfix-x-or [stack]
-  (let [b1 (first stack)
-        b2 (first (rest stack))]
-    (conj (rest (rest stack)) (or (and b1 (not b2)) (and b2 (not b1))))))
+(defn ^:private postfix-x-or [[b1 b2 & t]] (conj t (or (and b1 (not b2)) (and b2 (not b1)))))
 
-(defn postfix-not [stack] (conj (rest stack) (not (first stack))))
-
-(defn stack-unchanged [stack] (println (str "stack unchanged") stack) stack)
+(defn ^:private postfix-not [[b1 & t]] (conj t (not b1)))
 
 (defn expr-str->functions-on-stack [postfix-expression-str]
   (let [syntax-map {:1       push-true-val-to-stack
@@ -26,10 +19,13 @@
                     :A       postfix-and
                     :X       postfix-x-or
                     :N       postfix-not
-                    :default stack-unchanged}
-        post-fix-expr-minus-spaces-and-num-tokens-char (filter #(not= % \space) (rest postfix-expression-str))]
+                    }
+        post-fix-expr-minus-spaces-and-num-tokens-char (filter #(not= % \space) postfix-expression-str)]
     (map #((keyword (str %)) syntax-map) post-fix-expr-minus-spaces-and-num-tokens-char)))
 
 (defn eval [postfix-expression-str]
   (first (reduce #(%2 %1) (list) (expr-str->functions-on-stack postfix-expression-str))))
+
+
+
 
--- test/post_fix_booleans_clojure/boolean_postfix_test.clj
+++ test/post_fix_booleans_clojure/boolean_postfix_test.clj
@@ -10,11 +10,13 @@
         R for logical or,
         X for logical exclusive-or,
         N for logical negation."
-    (is (= true (boolean-postfix/eval "3 0 1 R")))
-    (is (= false (boolean-postfix/eval "3 0 0 R")))
-    (is (= true (boolean-postfix/eval "7 1 0 A 1 R N N")))
-    (is (= true (boolean-postfix/eval "9 0 0 A 0 N 0 N A R")))
-    (is (= false (boolean-postfix/eval "9 0 1 A 0 N 1 N A R")))
-    (is (= false (boolean-postfix/eval "9 1 0 A 1 N 0 N A R")))
-    (is (= true (boolean-postfix/eval "9 1 1 A 1 N 1 N A R")))
+    (is (= true (boolean-postfix/eval "0 1 R")))
+    (is (= false (boolean-postfix/eval "0 0 R")))
+    (is (= true (boolean-postfix/eval "1 0 A 1 R N N")))
+    (is (= true (boolean-postfix/eval "0 0 A 0 N 0 N A R")))
+    (is (= false (boolean-postfix/eval "0 1 A 0 N 1 N A R")))
+    (is (= false (boolean-postfix/eval "1 0 A 1 N 0 N A R")))
+    (is (= true (boolean-postfix/eval "1 1 A 1 N 1 N A R")))
+    (is (= true (boolean-postfix/eval "1 1 A 1 N 1 N A X")))
+    (is (= false (boolean-postfix/eval "1 1 A 0 N 0 N A X")))
     ))
